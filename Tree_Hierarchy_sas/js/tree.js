6d53759393ad06405b4fa3b5e6a709e56568b72c
ivyorca <55474425+ivyorca@users.noreply.github.com>
2019-11-19 15:05:08 +0800

    Update tree.js

--- Tree_Hierarchy_sas/js/tree.js
+++ Tree_Hierarchy_sas/js/tree.js
@@ -254,7 +254,7 @@ function updateChart() {
     .attr("transform", "translate(" + 50 + "," + 30 + ")");
 
 
-    stratify_data(g_sampleData);
+//     stratify_data(g_sampleData);
     // console.log(d_dataTable);
   dataTable = [];
   if (root3) {
